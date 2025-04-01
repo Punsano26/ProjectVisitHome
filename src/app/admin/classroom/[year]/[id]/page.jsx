8f8cf510b12187a2e4ecc6d9256c6bf9c4bab2a8
TanKittipat <kittipat12tan@gmail.com>
2025-04-01 21:47:27 +0700

    Fix some stupid bug

--- src/app/admin/classroom/[year]/[id]/page.jsx
+++ src/app/admin/classroom/[year]/[id]/page.jsx
@@ -1,137 +1,152 @@
 "use client";
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import { BiSolidEdit } from "react-icons/bi";
 import { AiOutlineDelete } from "react-icons/ai";
-import ModalAddStudent from '@components/modals/addstudent';
+import ModalAddStudent from "@components/modals/addstudent";
 
-const page = ({params}) => {
-  const {year, id} = params;
-  const [grade, setGrade] = useState("");
-  const [room, setRoom] = useState("");
+const page = ({ params }) => {
+  const { year, id } = params;
+  const [classroom, setClassroom] = useState(null);
+  console.log(classroom);
 
   useEffect(() => {
-   const fetchClassroom = async () => {
+    const fetchClassroom = async () => {
+      try {
+        const res = await fetch("/api/classroom");
+        const data = await res.json();
+        console.log("data", data);
 
-    const res = await fetch(`/api/classroom`)
-    .then(res => res.json())
-    if(res){
-      console.log("hello",res);
-      setGrade(res.data.grade)
-      setRoom(res.data.room)
-    }
-   }
-    fetchClassroom()
-  },[])
-  
+        setClassroom(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchClassroom();
+  }, []);
+
   return (
-    <div className='p-2 h-screen section-container'>
-         <h5 className="text-center font-bold">
-          มัธยมศึกษาปีที่<span className='text-green_light'> {grade} / {room}</span> 
-        </h5>
-         {/* ฟีเจอร์เสริม */}
-         <div className="flex items-center justify-between m-2 mt-4 mb-4">
-          {/* Dropdown ชิดซ้าย */}
-          <div className="dropdown dropdown-bottom mr-4">
-            <div tabIndex={0} role="button" className="btn">
-              ทั้งหมด
-            </div>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Item 2</a>
-              </li>
-            </ul>
+    <div className="p-2 h-screen section-container">
+      <h5 className="text-center font-bold">
+        มัธยมศึกษาปีที่
+        <span className="text-green_light">
+          {" "}
+          {classroom?.grade} / {classroom?.room}
+        </span>
+      </h5>
+      {/* ฟีเจอร์เสริม */}
+      <div className="flex items-center justify-between m-2 mt-4 mb-4">
+        {/* Dropdown ชิดซ้าย */}
+        <div className="dropdown dropdown-bottom mr-4">
+          <div tabIndex={0} role="button" className="btn">
+            ทั้งหมด
           </div>
-
-          {/* Search + Button ชิดขวา */}
-          <div className="flex items-center gap-4">
-            <label className="input">
-              <svg
-                className="h-[1em] opacity-50"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <circle cx="11" cy="11" r="8"></circle>
-                  <path d="m21 21-4.3-4.3"></path>
-                </g>
-              </svg>
-              <input type="search" required placeholder="กรอกชื่อนักเรียน" />
-            </label>
-            <button 
-            onClick={()=>document.getElementById('add_student').showModal()}
-            className="btn btn-success">เพิ่มชั้นเรียน</button>
-          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
         </div>
 
-        <div className='flex gap-4 justify-satart'> <h6>ครูที่ปรึกษา :<span>ณัฐวุฒ แฝงนาง</span></h6>
-        <h6>ข้อมูลติดต่อ :<span></span></h6></div>
-       
-       {/* ตารางแสดงรายชื่อนักเรียนขอห้องนี้ */}
-       <div className="overflow-x-auto ">
-                 <table className="table">
-                   {/* head */}
-                   <thead>
-                     <tr>
-                       <th>
-                         <label>
-                           <input type="checkbox" className="checkbox" />
-                         </label>
-                       </th>
-                       <th className="text-center">เลขที่</th>
-                       <th className="text-center">เลขที่ประจำตัวนักเรียน</th>
-                       <th className="">คำนำหน้า</th>
-                       <th>ชื่อ</th>
-                       <th>นามสกุล</th>
-                       <th className="text-center">แก้ไข/ลบ</th>
-                     </tr>
-                   </thead>
-                   <tbody>
-                     {/* row 1 */}
-                     <tr>
-                       <th>
-                         <label>
-                           <input type="checkbox" className="checkbox" />
-                         </label>
-                       </th>
-                       <td className="text-center">0</td>
-                       <td className="text-center">
-                         <span>43453</span>
-                       </td>
-                       <td className="">
-                         <span>นาย</span>
-                       </td>
-                       <td>
-                         <span>พันแสน</span>
-                       </td>
-                       <td>
-                         <span>สมกล้า</span>
-                       </td>
+        {/* Search + Button ชิดขวา */}
+        <div className="flex items-center gap-4">
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" required placeholder="กรอกชื่อนักเรียน" />
+          </label>
+          <button
+            onClick={() => document.getElementById("add_student").showModal()}
+            className="btn btn-success"
+          >
+            เพิ่มชั้นเรียน
+          </button>
+        </div>
+      </div>
 
-                       <td>
-                         <div className="flex gap-4 items-center justify-center">
-                           <BiSolidEdit />
-                           <AiOutlineDelete />
-                         </div>
-                       </td>
-                     </tr>
-                   </tbody>
-                 </table>
-               </div>
-               <ModalAddStudent/>
+      <div className="flex gap-4 justify-satart">
+        {" "}
+        <h6>
+          ครูที่ปรึกษา :<span>ณัฐวุฒ แฝงนาง</span>
+        </h6>
+        <h6>
+          ข้อมูลติดต่อ :<span></span>
+        </h6>
+      </div>
+
+      {/* ตารางแสดงรายชื่อนักเรียนขอห้องนี้ */}
+      <div className="overflow-x-auto ">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th className="text-center">เลขที่</th>
+              <th className="text-center">เลขที่ประจำตัวนักเรียน</th>
+              <th className="">คำนำหน้า</th>
+              <th>ชื่อ</th>
+              <th>นามสกุล</th>
+              <th className="text-center">แก้ไข/ลบ</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <td className="text-center">0</td>
+              <td className="text-center">
+                <span>43453</span>
+              </td>
+              <td className="">
+                <span>นาย</span>
+              </td>
+              <td>
+                <span>พันแสน</span>
+              </td>
+              <td>
+                <span>สมกล้า</span>
+              </td>
+
+              <td>
+                <div className="flex gap-4 items-center justify-center">
+                  <BiSolidEdit />
+                  <AiOutlineDelete />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <ModalAddStudent />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- src/app/api/classroom/route.js
+++ src/app/api/classroom/route.js
@@ -1,11 +1,9 @@
 export async function GET() {
-    return Response.json(
-        {
-          id: 1,  
-          grade: 1,
-          room: 1,
-          students: [],
-          teacher: "ครูณัฐวุฒิ"
-        }
-    )
+  return Response.json({
+    id: 1,
+    grade: 1,
+    room: 1,
+    students: [],
+    teacher: "ครูณัฐวุฒิ",
+  });
 }
